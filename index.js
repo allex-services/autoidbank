86989ad7b6d48819752c0568cb16348e0a954d68
Veljko Popovic <v.s.popovic@gmail.com>
2017-07-19 09:25:14 +0200

    updating dependencies

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ function createServicePack(execlib) {
   'use strict';
   return {
     service: {
-      dependencies: ['allex:bank', 'allex:leveldb:lib', 'allex:buffer:lib']
+      dependencies: ['allex_bankservice', 'allex:leveldb:lib', 'allex:buffer:lib']
     },
     sinkmap: {
-      dependencies: ['allex:bank']
+      dependencies: ['allex_bankservice']
     }, /*
     tasks: {
       dependencies: []
